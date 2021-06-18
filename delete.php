8ad3c09f691de81008255aeca4ebd59b906a63f0
rizalramlijr17 <58059501+rizalramlijr17@users.noreply.github.com>
2021-06-18 10:19:15 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,16 +2,16 @@
     include 'koneksi.php';
 
     $id = $_GET['id'];
-    $sql = "DELETE FROM `dosen` WHERE `dosen`.`id` = '".$id."`";
+    $sql = "DELETE FROM `dosen` WHERE `dosen`.`id` = '".$id."'";
     $query = mysqli_query($conn, $sql);
         if ($query) {
             $msg = "Data berhasil dihapus!";
         } else {
             $msg = "Data gagal dihapus!";
         }
-        $respone[] = array(
+        $respon[] = array(
             'status' => 'OK',
             'message' => $msg,
         );
-        echo json_encode($respone);
+        echo json_encode($respon);
 ?>
